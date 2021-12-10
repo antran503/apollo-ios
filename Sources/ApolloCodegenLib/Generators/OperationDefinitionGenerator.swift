7a1264abffcef9748aea6a06cdb34ad5971e04d9
Anthony Miller <AnthonyMDev@gmail.com>
2021-12-10 13:35:44 -0800

    Cleanup

--- Sources/ApolloCodegenLib/Generators/OperationDefinitionGenerator.swift
+++ Sources/ApolloCodegenLib/Generators/OperationDefinitionGenerator.swift
@@ -1,84 +1,5 @@
 import ApolloAPI
 
-struct Template: ExpressibleByStringInterpolation, CustomStringConvertible {
-
-  let value: String
-
-  init(stringLiteral: String) {
-    self.value = stringLiteral
-  }
-
-  init(stringInterpolation: StringInterpolation) {
-    self.value = stringInterpolation.output
-  }
-
-  var description: String { value }
-
-  struct StringInterpolation: StringInterpolationProtocol {
-    var output: String
-
-    init(literalCapacity: Int, interpolationCount: Int) {
-      var string = String()
-      string.reserveCapacity(literalCapacity)
-      self.output = string
-    }
-
-    mutating func appendLiteral(_ literal: StringLiteralType) {
-      output.append(literal)
-    }
-
-    mutating func appendInterpolation(_ template: Template) {
-      appendInterpolation(template.value)
-    }
-
-    mutating func appendInterpolation(_ string: String) {
-      let indent = output.reversed().prefix { " \t".contains($0) }
-      if indent.isEmpty {
-        appendLiteral(string)
-      } else {
-        let indentedString = string
-          .split(separator: "\n", omittingEmptySubsequences: false)
-          .joined(separator: "\n" + indent)
-
-        appendLiteral(indentedString)
-      }
-    }
-
-    mutating func appendInterpolation(if bool: Bool, _ string: String) {
-      if bool {
-        appendInterpolation(string)
-      } else {
-        removeLineIfEmpty()
-      }
-    }
-
-    mutating func appendInterpolation(if bool: Bool, _ template: Template) {
-      appendInterpolation(if: bool, template.value)
-    }
-
-    private mutating func removeLineIfEmpty() {
-      let slice = substringToStartOfLine()
-      if !slice.isEmpty && slice.allSatisfy(\.isWhitespace) {
-        output.removeLast(slice.count + 1) // + 1 removes the \n character.
-      }
-    }
-
-    private func substringToStartOfLine() -> Slice<ReversedCollection<String>> {
-      return output.reversed().prefix { !$0.isNewline }
-    }
-  }
-}
-
-//protocol ContextComponent {
-//  func toContextDictionary() -> String
-//}
-
-//extension CompiliationResult.OperationDefinition: ContextComponent {
-//  func toContextDictionary() -> String {
-//    ["source": ]
-//  }
-//}
-
 enum OperationDefinitionGenerator {
 
 //  static let templates = [
@@ -108,9 +29,10 @@ enum OperationDefinitionGenerator {
 
 }
 
+// MARK: - DocumentType
+
 extension OperationDefinitionGenerator {
   enum DocumentType {
-
     static func render(
       operation: CompilationResult.OperationDefinition,
       apq: ApolloCodegenConfiguration.APQConfig
@@ -126,7 +48,6 @@ extension OperationDefinitionGenerator {
         ""\"))
       """
     }
-
   }
 }
 
